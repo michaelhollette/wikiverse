56f44e47f0b28608110eb6b8fdd6c5a5db9e7391
Michael <131030279+michaelhollette@users.noreply.github.com>
2024-10-25 10:14:26 +0100

    Initial commit

---
+++ public/react/components/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { PagesList } from './PagesList'
+
+// import and prepend the api url to any fetch calls
+import apiURL from '../api'
+
+export const App = () => {
+  const [pages, setPages] = useState([])
+
+  useEffect(() => {
+    async function fetchPages () {
+      try {
+        const response = await fetch(`${apiURL}/wiki`)
+        const pagesData = await response.json()
+        setPages(pagesData)
+      } catch (err) {
+        console.log('Oh no an error! ', err)
+      }
+    }
+
+    fetchPages()
+  }, [])
+
+  return (
+		<main>
+      <h1>WikiVerse</h1>
+			<h2>An interesting 📚</h2>
+			<PagesList pages={pages} />
+		</main>
+  )
+}
